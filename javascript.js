f5706fd876ad3cb1c6080ceba83e1f7163ccd85d
tai bui <tai.c.bui@gmail.com>
2020-12-08 09:43:50 +0700

    first

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    alert('Xin chao');
+    <!--document.write('Good Morning');-->
+    document.getElementById("messageDiv").innerHTML = 'Hello';
+    console.log('Bong dua')
+}
